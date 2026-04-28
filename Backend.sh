e37c8a5b4228746f9e1b52a87f0d9cfca0312e2d
santhoshi <santhoshi6april@gmail.com>
2026-04-28 16:58:19 +0530

    Adding steps to remove old content and changing order of content in Backend

--- Backend.sh
+++ Backend.sh
@@ -2,6 +2,8 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
+cd Backend.service /etc/systemd/system/backend.service
+rm -rf/app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
@@ -9,7 +11,7 @@ unzip /tmp/backend.zip
 cd /app
 npm install
 
-cd Backend.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
