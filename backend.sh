9fbd020db010c1d56c06ac02bb0314557ded83ec
santhoshi <santhoshi6april@gmail.com>
2026-04-30 14:19:05 +0530

    Adding Heading and Exit Status

--- backend.sh
+++ backend.sh
@@ -1,19 +1,50 @@
+
+echo Disable default nodejs Version Module
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+echo$?
+
+echo Install Nodejs
 dnf install nodejs -y
+echo$?
+
+echo Adding Application User
 useradd expense
+echo$?
+
+echo copy Backend Service file
 cp Backend.service /etc/systemd/system/backend.service
+echo$?
+
+echo Clean up the old content
 rm -rf/app
+echo$?
+
+echo Create App Directory
 mkdir /app
+echo$?
+
+echo Download App Content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+echo$?
+
+echo Extract App Content
 cd /app
 unzip /tmp/backend.zip
+echo$?
+
+echo Download Nodejs Dependencies
 cd /app
 npm install
+echo$?
 
-
+echo enable-start backend
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
+echo$?
+
+echo Install mysql
 dnf install mysql -y
 mysql -h 172.31.40.213 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo$?
--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,22 @@
+echo install nginx
 dnf install nginx -y
+echo $?
+
+echo Start nginx
 systemctl enable nginx
 systemctl start nginx
+echo$?
+
 cp expence.conf /etc/nginx/default.d/expence.conf
+echo$?
+echo remove a file
 rm -rf /usr/share/nginx/html/*
+echo$?
+
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
+echo unzip
 unzip /tmp/frontend.zip
 
 systemctl restart nginx
+echo$?
