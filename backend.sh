34f0308b8a97c86837df35a0d9981cc3c05e81d4
santhoshi <santhoshi6april@gmail.com>
2026-05-02 17:22:45 +0530

    change in redirectors

--- backend.sh
+++ backend.sh
@@ -1,31 +1,31 @@
 echo "Disabling default nodejs Version Module"
-dnf module disable nodejs -y &>>$LOG_FILE
+dnf module disable nodejs -y &>/tmp/expense.log
 check_status "Disable nodejs module"
 
 echo "Enabling nodejs:20"
-dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>/tmp/expense.log
 check_status "Enable nodejs:20"
 
 echo "Installing Nodejs"
-dnf install nodejs -y &>>$LOG_FILE
+dnf install nodejs -y &>/tmp/expense.log
 check_status "Install nodejs"
 
 echo "Adding Application User"
-useradd expense &>>$LOG_FILE
+useradd expense &>/tmp/expense.log
 check_status "Add expense user"
 
 echo "Copying Backend Service file"
-cp backend.service /etc/systemd/system/backend.service &>>$LOG_FILE
+cp backend.service /etc/systemd/system/backend.service &>/tmp/expense.log
 check_status "Copy backend.service"
 
 echo "Cleaning up old content"
-rm -rf /app/* &>>$LOG_FILE
+rm -rf /app/* &>/tmp/expense.log
 check_status "Clean /app"
 
 echo "Creating App Directory"
-mkdir -p /app &>>$LOG_FILE
+mkdir -p /app &>/tmp/expense.log
 check_status "Create /app"
 
 echo "Downloading App Content"
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/backend-v2.zip &>>$LOG_FILE
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/backend-v2.zip&>/tmp/expense.log
 check_status "Download app content"
