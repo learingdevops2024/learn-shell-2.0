555bba021e5d110cdbfa9b1912391c722ba3ebcd
santhoshi <santhoshi6april@gmail.com>
2026-05-04 15:50:10 +0530

    Adding exit Status at every Operation ,,,

--- backend.sh
+++ backend.sh
@@ -2,21 +2,21 @@ LOG_FILE="/tmp/expense.log"
 
 echo "Disabling default nodejs Version Module"
 dnf module disable nodejs -y &>>$LOG_FILE
-check_status $?
+Check_Status $?
 
 echo "Enabling nodejs:20"
 dnf module enable nodejs:20 -y &>> "$LOG_FILE"
-check_status $?
+Check_Status $?
 
 echo "Installing Nodejs"
 dnf install nodejs -y &>> "$LOG_FILE"
-check_status $?
+Check_Status $?
 
 echo "Adding Application User"
 useradd expense &>> "$LOG_FILE"
 # Check if user already exists (grep returns 0 if found)
-check_status $?
+Check_Status $?
 
 echo "Copying Backend Service file"
 cp backend.service /etc/systemd/system/backend.service &>> "$LOG_FILE"
-check_status $?
+Check_Status $?
--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-check_status() {
+Check_Status() {
   if [ $1 -eq 0 ]; then
     echo SUCCESS
   else
