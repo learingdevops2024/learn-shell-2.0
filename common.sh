ac7f25f3c5df6a1e99180eeb2879d2630263c448
santhoshi <santhoshi6april@gmail.com>
2026-05-06 16:05:48 +0530

    change ./ check_status

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 LOG_FILE="/tmp/expense.log"
 
-Check_Status() {
+check_status() {
   if [ $1 -eq 0 ]; then
     echo SUCCESS
   else
@@ -11,18 +11,18 @@ Check_Status() {
 App_PreReq() {
   Print_Task_Heading "Clean the Old Content"
   rm -rf ${app_dir} &>>$LOG_FILE
-  Check_Status $?
+ check_status $?
 
   Print_Task_Heading "Create App Directory"
   mkdir ${app_dir} &>>$LOG_FILE
-  Check_Status $?
+  check_status $?
 
   Print_Task_Heading "Download App Content"
   curl -o /tmp/${component}.zip https://expense-artifacts.s3.amazonaws.com/expense-${component}-v2.zip &>>$LOG_FILE
-  Check_Status $?
+ check_status $?
 
   Print_Task_Heading "Extract App Content"
   cd ${app_dir} &>>$LOG_FILE
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  Check_Status $?
+  check_status $?
 }
--- frontend.sh
+++ frontend.sh
@@ -4,15 +4,15 @@ component=frontend
 
 Print_Task_Heading "install nginx"
 dnf install nginx -y &>>$LOG_FILE
-Check_Status $?
+check_status $?
 
 Print_Task_Heading "Copy Expence Nginx File"
 cp expence.conf /etc/nginx/default.d/expence.conf &>>$LOG_FILE
-Check_Status $?
+check_status $?
 
 App_PreReq
 
 Print_Task_Heading "Restart nginx"
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
-Check_Status $?
+check_status $?
